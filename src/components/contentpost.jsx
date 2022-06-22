9f996ddd8136c5743adcb2d39190d330e25f5e62
venusai <venusaivenu12012002@gmail.com>
2022-06-23 01:06:02 +0530

    updated

--- src/components/contentpost.jsx
+++ src/components/contentpost.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import './contentpost.css'
 import propic from '../images/profilepic.jpg'
+import Send from "../images/send.png"
 function Contentpost() {
   return (
     <div className='contentpost'>
         <img className='round' src={propic} alt="" />
-        <div>
-          hey there welcome to git hubby
+        <div className='postinginput'>
+            <div className='inputs'>
+              <input className='bar' type="text" placeholder="WHats going on..." />
+              <img src={Send} className="sendicon" alt="" />
+            </div>
+            
+            <div className='icons'>
+                <div className='cons'>
+            
+                  images
+                </div>
+                <div className='cons'>
+                  videos
+                </div>
+                <div className='cons'>
+                  location
+                </div>
+                <div className='cons'>
+                  scedule
+                </div>
+            </div>
         </div>
     </div>
     
